e0673d5426dbcbc733c508975b6c8bcd9c7790e2
justinaiken7 <60617740+justinaiken7@users.noreply.github.com>
2020-05-13 10:48:11 -0400

    Update app.js
    If the table isnt empty
    Loop through table

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 var tbl = document.getElementsByTagName(`table`)[0];
+if (tbl != null) 
+	{
+    for (var i = 0; i < tbl.rows.length; i++) 
+		{
+        for (var j = 0; j < tbl.rows[i].cells.length; j++) 
+			{
+            tbl.rows[i].cells[j].onclick = function() 
+				{
+                performRotation(this);
+				};
+			}
+		}
+	}
+
